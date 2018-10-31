cd369e187124c9d8f0900dc431ac87f4ad60628a
matthew-sword <lucklgh@126.com>
2018-10-31 18:39:03 +0800

    merge from master

--- display.h
+++ display.h
@@ -3,30 +3,8 @@
 #include<stdio.h>
 #include<string.h>
 #include "rich.h"
-typedef enum
-{
-    RED0_Q = 96, 
-    RED1_Q,
-    RED2_Q, 
-    RED3_Q,
-    GREEN0_A, 
-    GREEN1_A,
-    GREEN2_A,
-    GREEN3_A,
-    BLUE0_S, 
-    BLUE1_S, 
-    BLUE2_S,
-    BLUE3_S,
-    YELLOW0_J,
-    YELLOW1_J,
-    YELLOW2_J,
-    YELLOW3_J
-}COLOR;
-/* change the value of position with short_name */
-void oneD2twoD(char position, char short_name);
-/* print map ,param [in] = map */
-void printMap(char line[], char position, char short_name);
-void display(char position, char short_name);
+
+void display(MAP* maps);
 
 void print_red(char *chars);
 void print_green(char *chars);
